347594071b0e3ce0c7d5baa4e4e05a8e9da0111c
Jonas Mattsson <jonas.mattsson@verisure.com>
2019-09-16 16:40:16 +0200

    changed connection string to local db

--- database.js
+++ database.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const initDB = () => {
 
-  mongoose.connect('mongodb://indrek1:indrek1indrek1@ds213615.mlab.com:13615/koa-graphql', { useNewUrlParser: true });
+  'mongodb://localhost/admin',
+  mongoose.connect('mongodb://localhost/admin', { useNewUrlParser: true });
+  // mongoose.connect('mongodb://indrek1:indrek1indrek1@ds213615.mlab.com:13615/koa-graphql', { useNewUrlParser: true });
   mongoose.connection.once('open', () => {
     console.log('connected to database');
   });
---
+++ readme.txt
@@ -0,0 +1,8 @@
+https://medium.com/better-programming/how-to-setup-a-powerful-api-with-graphql-koa-and-mongodb-339cfae832a1
+https://github.com/indreklasn/koa-graphql-mongodb/tree/part-1
+
+startup: npm start
+
+Problems with packages not found?
+rm -rf node_modules
+npm i
